d1a5969b5209034ea91334ffc548cd446e930bed
mauricio403 <mauricioatango_@hotmail.com>
2021-04-14 10:47:46 -0500

    operadores =+

--- inicio.py
+++ inicio.py
@@ -1,6 +1,25 @@
-
 print('Hola mundo')
+
 #imprimimos todas las palabras reservadas de python
 
 import keyword
 print(keyword.kwlist)
+
+#declarando multiples variables en una sola linea
+
+age, name = 22, "Maria"
+
+print(age)
+print(name)
+
+
+#operaciones con mutiliples variables numerico
+
+x =3
+y = x + 1  #otra opcion es x +=2
+
+print(y)
+
+#otros que son igual, tenemos
+
+# -= , *= , /= , //= , %= y **=
