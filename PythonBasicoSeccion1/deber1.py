231a7c8e014412481dd0119f58a42144168ef222
Mauricio Matango <Mauriciomatango_@hotmail.com>
2021-04-14 11:42:50 -0500

    Division en secciones y deber de la seccion1

---
+++ PythonBasicoSeccion1/deber1.py
@@ -0,0 +1,11 @@
+name = "Pyratilla"
+age = 17
+pet = True
+pet_name = "Pyo"
+ocupation = "Pirata" # Y en cuanto tenga tripulación, seré capitán
+coins = 0
+
+#recibe 100 monedas, entonces cree la  linea de codigo que actualiza dicha o[cion]
+
+coins +=100
+print(coins)
--- PythonBasicoSeccion1/inicio.py
+++ PythonBasicoSeccion1/inicio.py
@@ -23,3 +23,27 @@ print(y)
 #otros que son igual, tenemos
 
 # -= , *= , /= , //= , %= y **=
+
+
+#darle un alias a una libreria importafa
+
+#import math as matxd
+
+#importar solo la libreria que me interesa
+
+#from math import pi
+
+#print(pi)
+
+
+#Para descargar todas las opciones de una libreria use:
+
+#from math import *
+
+x = 20 # Aquí está nuestra variable x
+# Ahora vamos a restarle 2 y dividir por la mitad
+y = x - 2
+x /= 2
+print(y)
+
+
